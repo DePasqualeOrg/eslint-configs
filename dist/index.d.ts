fd197291f1fae7ee80599166396bedad45b36d0a
Anthony DePasquale <anthony@depasquale.org>
2025-09-09 08:43:44 +0200

    Fix source maps

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import type { Linter } from 'eslint';
 export declare const typeScriptConfig: Linter.Config;
 export declare const svelteKitConfigs: Linter.Config[];
+//# sourceMappingURL=index.d.ts.map
